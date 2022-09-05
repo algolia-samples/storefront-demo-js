68c1dfc5c3c829c3ea155b452f1adb11453544bb
Dhaya <154633+dhayab@users.noreply.github.com>
2022-09-05 16:56:36 +0200

    feat: build home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./resources/**/*.blade.php"],
+    content: ["./resources/**/*.blade.php", "./storage/app/mock.json"],
     theme: {
         extend: {},
     },
